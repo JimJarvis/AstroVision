88821096ef9fae099a000615a462131585cff8f0
JimJarvis <jimfanspire@gmail.com>
2014-04-12 22:23:39 -0400

    Comment loadAllFeature usage

--- loadAllFeatures.m
+++ loadAllFeatures.m
@@ -1,18 +1,18 @@
 %% Loads images from all folders, computes features and stores them
-% option struct as specified in optionReader
+% Gaussian pyramid options must be consistent with how the BIN_*.mat are generated
 %
 function datamap = loadAllFeatures()
 
-fName = @(folder) ['data\' folder];
-% folders = {'ref', 'si850', 'w12', 'om23'};
 folders = {'ref', 'si850', 'w12'};
+
+fName = @(folder) ['data/' folder];
 datamap = containers.Map(); % matlab hashmap
 
 for folder = folders  
     folder = folder{1};
     fprintf('\n=========== %s ===========\n', folder);
     loaded = loadVar(['BIN_' folder]);
-    opt.bincell = loaded{1}
+    opt.bincell = loaded{1};
     datamap(folder) = ...
         batchFeatureFolder(fName(folder), opt);
 end
