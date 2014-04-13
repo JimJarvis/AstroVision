494a8534bee66ec6f7a7efb5b9ede968ffd94172
JimJarvis <jimfanspire@gmail.com>
2014-04-13 10:53:46 -0400

    Fix saving bug in loadAllFeatures

--- loadAllFeatures.m
+++ loadAllFeatures.m
@@ -1,20 +1,21 @@
 %% Loads images from all folders, computes features and stores them
 % Gaussian pyramid options must be consistent with how the BIN_*.mat are generated
 %
-function datamap = loadAllFeatures()
+function featureMap = loadAllFeatures()
 
 folders = {'ref', 'si850', 'w12'};
 
 fName = @(folder) ['data/' folder];
-datamap = containers.Map(); % matlab hashmap
+featureMap = containers.Map(); % matlab hashmap
 
 for folder = folders  
     folder = folder{1};
     fprintf('\n=========== %s ===========\n', folder);
     loaded = loadVar(['BIN_' folder]);
     opt.bincell = loaded{1};
-    datamap(folder) = ...
+    featureMap(folder) = ...
         batchFeatureFolder(fName(folder), opt);
 end
-save('AstroBase', datamap);
+
+save('AstroFeatures', 'featureMap');
 fprintf('\nDONE\n');
