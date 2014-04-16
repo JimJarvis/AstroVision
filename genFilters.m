569ee695988119cfbcc60590635dda9e9e1f7214
JimJarvis <jimfanspire@gmail.com>
2014-04-16 17:10:38 -0400

    Deprecate the optionReader. Use genFilters() instead. imgBinRange udpated

---
+++ genFilters.m
@@ -0,0 +1,39 @@
+%% Generate a pyramid of gaussian filters
+% Parameter: option struct
+% .level: pyramid level [6]
+% .window: gaussian window size [7]
+% .sigma: initial sigma [1]
+% .scale: sigma * scale^n, should always be > 1 [2]
+%
+% .img: apply the filters on the image and save the pyramid stack to disk
+% .save: file name for the saved stack
+%
+function filters = genFilters(option)
+
+if isfield(option, 'level'), level = option.level; else level = 6; end
+if isfield(option, 'window'), window = option.window; else window = 7; end
+if isfield(option, 'sigma'), sigma = option.sigma; else sigma = 1; end
+if isfield(option, 'scale'), scale = option.scale; else scale = 2; end
+
+filters = cell(level, 1);
+
+% Leave filter{1} empty because the first level is the original image
+for l = 2:level
+    filters{l} = fspecial('gaussian', window, sigma * scale^(l-2));
+end
+
+if isfield(option, 'img')
+    if ~isfield(option, 'save')
+        error('You must specify a file name for saving');
+        return
+    end
+    for l = 1:level
+        if isempty(filters{l})
+            img = option.img;
+        else
+            img = imfilter(option.img, filters{l});
+        end
+        imwrite(uint8(img), ...
+            sprintf('test/%s_%d.png', option.save, l));
+    end
+end
--- imgBinRange.m
+++ imgBinRange.m
@@ -1,16 +1,24 @@
 %% Get 1D-clustered bin ranges from the image pyramids of a few randomly chosen images in a specific folder
 % parameter: 
 % - folder: folder of the images
+% - nBin: how many histogram bins per level. If a single number, we use the same nBin for all levels
 % - nSample: how many samples to draw from the folder to do the 1D clustering
-% - option: specify various options using optionReader()
-% - saveName: leave it blank if you don't want to save the result to disk. Otherwise specify this name and will be saved (or appended) to BIN_<saveName>.mat with the variable name bin_<saveName>_<nbin>
+% - option: as specified in genFilters. Leave it [] to use defaults.
+% - saveName: leave it blank if you don't want to save the result to disk. Otherwise specify this name and will be saved (or appended) to BIN_<saveName>.mat with the variable name bin_<saveName>_<nBin>
 %
 % returns:
-% - bincell: a cell array whose components are the bin ranges of each level of the pyramid. Each bin range will have (nbin - 1) interval endpoints, omitting -Inf at beginning and +Inf at end. 
+% - bincell: a cell array whose components are the bin ranges of each level of the pyramid. Each bin range will have (nBin - 1) interval endpoints, omitting -Inf at beginning and +Inf at end. 
 %
-function bincell = imgBinRange(folder, nSample, option, saveName)
+function bincell = imgBinRange(folder, nBin, nSample, option, saveName)
 
-[level, window, sigma, scale, nbin] = optionReader(option);
+filters = genFilters(option);
+level = numel(filters);
+
+if numel(nBin) == 1
+    nBin = nBin * ones(level);
+elseif numel(nBin) ~= level
+    error('levels of nBin must agree with levels of filters');
+end
 
 files = getFilesFolder(folder); % listing of all file names
 nFile = numel(files);
@@ -20,24 +28,22 @@ randImg = @() mat2gray(fitsread([folder '/' files{randi(nFile)}]));
 % Randomly choose 10 images to do the 1D clustering for each level of the pyramid. Report their GVF as well
 bincell = cell(level, 1);
 
-for l = 1:level % l == 1 for the original unblurred image
+for l = 1:level % l == 1 for the original unblurred image (filter will be empty)
     mergedImg = [];
     % multi-resolution
-    if l ~= 1
-        gfilter = fspecial('gaussian', window, sigma * scale^(l-2)); end
     for s = 1:nSample
         img = randImg();
-        if l ~= 1
-            img = imfilter(img, gfilter); end
+        if ~isempty(filters{l})
+            img = imfilter(img, filters{l}); end
         mergedImg = [mergedImg; img(:)];
     end
-    bincell{l} = lloyd1D(mergedImg, nbin(l));
+    bincell{l} = lloyd1D(mergedImg, nBin(l));
 end
 
 %% Save to disk
 if exist('saveName', 'var')
     fileName = ['BIN_' saveName '.mat'];
-    varName = ['bin_' saveName '_' num2str(nbin(1))];
+    varName = ['bin_' saveName '_' num2str(nBin(1))];
     eval([varName '=bincell;']);
     if exist(fileName, 'file')
         eval(['save ' fileName ' ' varName ' -append']);
